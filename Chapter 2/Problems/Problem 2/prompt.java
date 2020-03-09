8bf2fa90282a1582addc1a259f3e5ee3409ebf80
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 14:15:50 -0500

    Create prompt.java

---
+++ Chapter 2/Problems/Problem 2/prompt.java
@@ -0,0 +1,13 @@
+/*
+Look into the API documentation of the String class and locate the trim method. 
+Write a program demonstrating what it does. 
+Then show how you can use the replace method to remove all spaces from a string. 
+The output should look like:
+This program demonstrates the behavior of String's trim() function.
+The starting string is 
+  Here is a string   with  multiple spaces   
+And here it is after applying the trim function
+Here is a string   with  multiple spaces
+And after removing all spaces using the replace function:
+Hereisastringwithmultiplespaces
+*/
