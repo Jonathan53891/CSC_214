6cd6d81c0538f010d51abb1ff2ff79171190b344
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 14:29:01 -0500

    Create prompt.java

---
+++ Chapter 3/Problems/Problem 4/prompt.java
@@ -0,0 +1,9 @@
+/*
+
+ Implement a class Product. A product has a name and a price, for example 
+ new Product("Toaster", 29.95). Supply methods getName, getPrice, and reducePrice. 
+
+Using class Main, supply code that makes two products, prints each name and price, 
+reduces their prices by $5.00, and then prints the prices again. 
+
+*/
