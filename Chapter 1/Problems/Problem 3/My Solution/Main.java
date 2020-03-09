72473fb8f26ac835ed48ac667cf3d515220ea140
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 13:37:18 -0500

    Create Main.java

---
+++ Chapter 1/Problems/Problem 3/My Solution/Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+      System.out.print("Balance after year 1 = $");
+        System.out.println(1000*1.05);
+        // Add statement to print amount of balance after year 1
+        System.out.print("Balance after year 2 = $");
+        // Add statement to print amount of balance after year 2
+        System.out.println(1050*1.05);
+        System.out.print("Balance after year 3 = $");
+        System.out.println(1102.5*1.05);
+        // Add statement to print amount of balance after year 3
+  }
+}
