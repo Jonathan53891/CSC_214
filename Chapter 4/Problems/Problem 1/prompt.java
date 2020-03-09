d6aa1fe60e387cb5ffddce0baa94f131dcc6be1c
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 14:41:37 -0500

    Create prompt.java

---
+++ Chapter 4/Problems/Problem 1/prompt.java
@@ -0,0 +1,12 @@
+/*
+
+Write a program that displays the properties of a letter-size (8.5 × 11 inches) 
+sheet of paper in millimeters. There are 25.4 millimeters per inch. The program should print:
+
+The width and height
+The perimeter
+The area
+
+Use constants and comments in your program.
+
+*/
