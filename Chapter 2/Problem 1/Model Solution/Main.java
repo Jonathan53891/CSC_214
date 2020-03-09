3c83af517f6fd334cfc329d98e8054b27a9c83ea
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 14:07:54 -0500

    Update Main.java

--- Chapter 2/Problem 1/Model Solution/Main.java
+++ Chapter 2/Problem 1/Model Solution/Main.java
@@ -1,1 +1,14 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+      String state = "Mississippi";
+        
+        state = state.replace("i", "ii");
+        System.out.println("Length of new string: " + state.length());
+        
+        state = state.replace("ss", "s");
+        System.out.println("Length of new string: " + state.length());
+  }
+}
 
