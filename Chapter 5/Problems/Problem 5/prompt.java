436f4a776772de79a13f680168aeeb8a849a83d5
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 15:03:12 -0500

    Create prompt.java

---
+++ Chapter 5/Problems/Problem 5/prompt.java
@@ -0,0 +1,24 @@
+/*
+
+Write a program that takes user input describing a playing card in the 
+following shorthand notation:
+A -> Ace
+2..10 -> Face Vaue
+J->Jack
+Q->Queen
+K->King
+D->Diamonds
+H->Hearts
+S->Spades
+C->Clubs
+
+Your program should print the full description of the card. For example,
+Enter the card notation: QS 
+Queen of Spades
+
+Implement a class Card whose constructor takes the card notation string 
+and whose getDescription method returns a description of the card. If the 
+notation string is not in the correct format, the getDescription method 
+should return the string "Unknown".
+
+*/
