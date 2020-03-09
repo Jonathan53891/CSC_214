0d1800d49194a1d9c8efa6a544e067b15173d895
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 13:39:41 -0500

    Create Main.java

---
+++ Chapter 1/Problems/Problem 4/My Solution/Main.java
@@ -0,0 +1,9 @@
+class Main {
+  public static void main(String[] args) {
+       System.out.println("The sum  of the first 10 positive integers is:");
+        System.out.print("1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 = ");
+        System.out.print(1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10);
+        // write your code hear to finish the problem.
+        
+  }
+}
