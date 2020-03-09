d62ef07582e0a034766b9f5c47ee2a59c80c2fb0
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 13:36:49 -0500

    Create prompt.java

---
+++ Chapter 1/Problems/Problem 3/prompt.java
@@ -0,0 +1,11 @@
+/*
+
+Write a program that prints the balance of an account after the first, second, and third year. 
+The account has an initial balance of $1,000 and earns 5 percent interest per year.
+
+Note. By default a calculation ending in 0 such as 1000.00 will display as 1000.0.  
+Likewise, something ending in more than 2 digits like .325 wont be rounded but will display as .325
+
+Leave these for now.  We will address how to make them prettier later.
+
+*/
