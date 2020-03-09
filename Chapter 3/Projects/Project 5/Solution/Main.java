69766b256bb99fe97afea88e8d8f831698a1a61c
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 14:38:46 -0500

    Create Main.java

---
+++ Chapter 3/Projects/Project 5/Solution/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+   public static void main(String[] args) {
+       Greeter g = new Greeter("Dave");
+       System.out.println(g.sayHello());
+       System.out.println(g.sayGoodbye());
+       System.out.println(g.refuseHelp());
+   }
+}
