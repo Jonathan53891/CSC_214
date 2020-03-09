ea63c3c81af6fc95f2a02cbf27544d605f22804d
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 14:13:54 -0500

    Update prompt.java

--- Chapter 2/Problems/Problem 1/prompt.java
+++ Chapter 2/Problems/Problem 1/prompt.java
@@ -1,1 +1,14 @@
+/*
+
+Write a program that initializes a string with "Mississippi". 
+Then replace all "i" with "ii" and print the length of the resulting string. 
+In that string, replace all "ss" with "s" and print the length of the resulting string. 
+
+Your output should look like:
+
+Length of new string: xx
+Length of new string: xx
+
+*/
+
 
