cd37b0ca33befdc5dc22bed95fb9ee5e6ec3f400
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 13:34:49 -0500

    Update Main.java

--- Chapter 1/Problems/Problem 2/My Solution/Main.java
+++ Chapter 1/Problems/Problem 2/My Solution/Main.java
@@ -1,1 +1,11 @@
-
+class Main {
+    public static void main(String[] args) 
+    {
+        System.out.println("The product of the first 10 positive integers is:");
+        System.out.print("1 x 2 x 3 x 4 x 5 x 6 x 7 x 8 x 9 x 10 = ");
+        System.out.print(1*2*3*4*5*6*7*8*9*10);
+		// Add a statement here to display the product of the 10 integers
+		// and a newline
+        
+    }
+}
