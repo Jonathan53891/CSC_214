f8d8870f920db0a7ca512d4f5c81f96443b0ad77
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 14:55:44 -0500

    Create prompt.java

---
+++ Chapter 5/Problems/Problem 1/prompt.java
@@ -0,0 +1,18 @@
+/*
+
+Write a program that reads an integer and prints whether it is negative, zero, or positive.
+
+Output should look like:
+ 
+Enter a number:  3
+It's a positive number
+
+ 
+Enter a number:  0
+It's a 0
+
+ 
+Enter a number:  -5
+It's a negative number
+
+*/
