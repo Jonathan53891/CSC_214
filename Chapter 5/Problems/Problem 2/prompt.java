7385f5eaf86176dd70ce32f956fb2d55383c2cdb
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 14:57:49 -0500

    Create prompt.java

---
+++ Chapter 5/Problems/Problem 2/prompt.java
@@ -0,0 +1,8 @@
+/*
+
+ Write a program that reads an integer and prints how many digits the number 
+ has, by checking whether the number is ≥ 10, ≥ 100, and so on. (Assume that 
+ all integers are less than ten billion.) If the number is negative, first 
+ multiply it with –1. 
+
+*/
