425078ba873b63831d570b48adce10d6668b77ba
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 13:42:39 -0500

    Create Main.java

---
+++ Problems/Problem 1/Model Solution/Main.java
@@ -0,0 +1,13 @@
+class Main 
+{
+  public static void main(String[] args) 
+  {
+      String state = "Mississippi";
+        
+        state = state.replace("i", "ii");
+        System.out.println("Length of new string: " + state.length());
+        
+        state = state.replace("ss", "s");
+        System.out.println("Length of new string: " + state.length());
+  }
+}
