2e9987751b1b785106f219fc31ace47eb780dc26
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-09 14:21:53 -0500

    Create prompt.java

---
+++ Chapter 3/Problems/Problem 1/prompt.java
@@ -0,0 +1,30 @@
+/*
+
+Simulate a tally counter that can be used to admit a limited number of people. 
+First, the limit is set with a call
+
+public void setLimit(int maximum)
+
+If the click button is clicked more often than the limit, it has no effect. 
+
+(Hint: The call Math.min(n, limit) returns n if n is less than limit, and limit otherwise.)
+
+Break your solution into 2 classes.  
+Main which tests the class, and Counter which is where the functionality is implemented.
+
+The contents of Main, which is a test driver, have been given.
+
+
+NOTE: TO KEEP THEM BOTH IN THE SAME FILE DO NOT USE PUBLIC IN FRONT OF THEM
+
+That is not required in a non repl system or in the projects.  But in a single file system it is.
+
+Expected output:
+ 
+3
+Expected: 3
+3
+Expected: 3
+
+
+*/
