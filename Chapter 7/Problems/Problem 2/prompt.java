795e5d28e8acc4d4d3093214b9f5ec49c823a527
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-12 17:36:41 -0500

    Update prompt.java

--- Chapter 7/Problems/Problem 2/prompt.java
+++ Chapter 7/Problems/Problem 2/prompt.java
@@ -1,1 +1,7 @@
+Write a program that initializes an array with ten random integers and then 
+prints four lines of output, containing 
 
+- Every element at an even index. 
+- Every even element. 
+- All elements in reverse order. 
+- Only the first and last element. 
