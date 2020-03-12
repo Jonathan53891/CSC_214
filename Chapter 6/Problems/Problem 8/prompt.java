9a1d1e25ccb7f669d931b5e8615334ba0ccb0ded
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-12 12:10:07 -0500

    Create prompt.java

---
+++ Chapter 6/Problems/Problem 8/prompt.java
@@ -0,0 +1,11 @@
+Write a program that reads an integer and displays, using asterisks, a filled diamond of the given side length. For example, if the side length is 4, the program should display
+
+Remember, the sides are what the length refers to.  Not the number of stars in a row.
+
+   *
+  ***
+ *****
+*******
+ *****
+  ***
+   *
