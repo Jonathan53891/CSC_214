3305baf47e86caeec65d97c36c3740a61667d7b2
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-15 20:46:38 -0500

    Create prompt.java

---
+++ Chapter 7/Problems/Problem 3/prompt.java
@@ -0,0 +1,17 @@
+/*
+
+Write a class DataSet that stores a number of values of type double. Provide a constructor
+
+public DataSet(int maximumNumberOfValues)
+
+and a method
+
+public void add(double value)
+
+that adds a value to the dataset, provided there is still room.
+
+Provide methods to compute the sum, average, maximum, and minimum value.
+
+Provide a tester for the above requirements.
+
+*/
