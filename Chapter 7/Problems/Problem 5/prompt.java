394ba41a7e142275bd83e2647a0419eb5e39f335
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-22 17:29:54 -0500

    Create prompt.java

---
+++ Chapter 7/Problems/Problem 5/prompt.java
@@ -0,0 +1,7 @@
+Write a program that:
+Generates a sequence of 20 random values between 0 and 99 in an array
+Prints the sequence
+Sorts the arrray
+Prints the sorted sequence. 
+
+Use the sort method from the standard Java library 
