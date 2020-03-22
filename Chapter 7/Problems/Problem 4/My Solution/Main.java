4f29d86ca6a1a754db60b58984a6d7749bfcfcc0
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-22 17:28:38 -0500

    Create Main.java

---
+++ Chapter 7/Problems/Problem 4/My Solution/Main.java
@@ -0,0 +1,42 @@
+class Main {
+  public static void main(String[] args) {
+   
+  }
+}
+
+/*
+public class Sequence
+{
+  private int[] values;
+  public Sequence(int size) { values = new int[size]; }
+  public void set(int i, int n) { values[i] = n; }
+  public int get(int i) { return values[i]; }
+  public int size() { return values.length; }
+  public boolean sameValues(Sequence other){
+    boolean flag;
+    for(int i = 0;i<size();i++){
+      flag = false;
+      for(int j = 0;j<other.size();j++){
+        if(get(i)==other.get(j)){
+          flag = true;
+        }
+      }
+      if(!flag){
+        return false;
+      }
+    }
+    for(int j = 0;j<other.size();j++){
+      flag = false;
+      for(int i = 0;i<size();i++){
+        if(get(i)==other.get(j)){
+          flag = true;
+        }
+      }
+      if(!flag){
+        return false;
+      }
+    }
+    return true;
+  }
+}
+*/
