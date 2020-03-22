7a317a58b35aaf7dcbb508bb51932817e41cc2b8
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-22 17:33:26 -0500

    Create Main.java

---
+++ Chapter 7/Problems/Problem 7/My Solution/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main 
+{
+  public static void main(String[] args) 
+  {
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter the size of the square (must be odd): ");
+    int size = in.nextInt();
+    if (size % 2 == 1) 
+    {
+      MagicSquare mySquare = new MagicSquare(size);
+      System.out.println(mySquare.toString());
+    } 
+    else 
+    {
+      System.out.println("Size must be odd.");
+    }
+  }
+}
