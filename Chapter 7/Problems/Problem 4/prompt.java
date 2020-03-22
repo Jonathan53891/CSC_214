45bd970a3b0e161f3b87ab64902099364bd656f2
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-03-22 17:29:08 -0500

    Update prompt.java

--- Chapter 7/Problems/Problem 4/prompt.java
+++ Chapter 7/Problems/Problem 4/prompt.java
@@ -1,1 +1,17 @@
+/*
+Consider the following class:
 
+public class Sequence
+{
+  private int[] values;
+  public Sequence(int size) { values = new int[size]; }
+  public void set(int i, int n) { values[i] = n; }
+  public int get(int i) { return values[i]; }
+  public int size() { return values.length; }
+}
+
+Add a method
+public boolean equals(Sequence other)
+
+that checks whether two sequences have the same values in the same order.
+*/
