1c1fce533505a6ae9d0ac3f33478e3114170f9ed
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 12:01:21 -0500

    Create Employee.java

---
+++ Chapter 9/Problems/Problem 1/Solution/Employee.java
@@ -0,0 +1,26 @@
+class Employee 
+{
+  private String name;
+  private double baseSalary;
+  
+  public void setName(String newName) 
+  {
+    name = newName;
+  }
+
+  public void setBaseSalary(double newSalary) 
+  {
+    baseSalary = newSalary;
+  }
+
+  public String getName() 
+  {
+    return name;
+  }
+
+  public double getSalary() 
+  {
+    return baseSalary;
+  }
+  
+}
