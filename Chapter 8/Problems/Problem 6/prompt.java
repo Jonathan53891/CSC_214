1db0fc69a361b8ea86603287e0d32987951d70da
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 11:58:43 -0500

    Create prompt.java

---
+++ Chapter 8/Problems/Problem 6/prompt.java
@@ -0,0 +1,8 @@
+Implement a program that prints paychecks for a group of student assistants. 
+
+Deduct federal and Social Security taxes. (You may want to use the tax 
+computation used in Chapter 5. Find out about Social Security taxes on the 
+Internet.) 
+
+Your program should prompt for the names, hourly wages, and hours worked of 
+each student. 
