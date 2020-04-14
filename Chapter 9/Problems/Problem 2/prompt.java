8ac1e70d8f013e7f11211c1cc8ef8ee7b2649356
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 12:01:52 -0500

    Create prompt.java

---
+++ Chapter 9/Problems/Problem 2/prompt.java
@@ -0,0 +1,10 @@
+Create a simple class, BankAccount with methods:
+
+Deposit
+Withdraw
+GetBalance 
+
+Implement a subclass of BankAccount called BasicAccount whose withdraw 
+method will not withdraw more money than is currently in the account. 
+
+Implement a test class as well
