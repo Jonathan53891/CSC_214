347e38a4e206cfbf151b741b919d2f506dadb08a
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 12:00:24 -0500

    Create prompt.java

---
+++ Chapter 9/Problems/Problem 1/prompt.java
@@ -0,0 +1,16 @@
+Suppose the class Employee is declared as follows: 
+
+public class Employee 
+{ 
+     private String name; 
+     private double baseSalary; 
+     public void setName(String newName) { . . . } 
+     public void setBaseSalary(double newSalary) { . . . } 
+     public String getName() { . . . } 
+     public double getSalary() { . . . }
+} 
+
+Declare a class Manager that inherits from the class Employee and adds an 
+instance variable bonus for storing a salary bonus. 
+
+Implement the constructors and methods, and supply a test program. 
