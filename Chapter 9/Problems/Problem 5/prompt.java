7b07fcc229adb69df0f9aebad04c399697622a63
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 12:07:34 -0500

    Create prompt.java

---
+++ Chapter 9/Problems/Problem 5/prompt.java
@@ -0,0 +1,14 @@
+Implement a superclass Appointment and subclasses Onetime, Daily, and Monthly. 
+
+An appointment has a description (for example, “see the dentist”) and occurs 
+on one or more dates. 
+
+Write a method occursOn(int year, int month, int day) that checks whether the 
+appointment occurs on that date. For example, for a monthly appointment, you 
+must check whether the day of the month matches. 
+
+Then fill an array of Appointment objects with a mixture of appointments. 
+Have the user enter a date and print out all appointments that occur on that 
+date. 
+
+This requires the use of polymorphism.
