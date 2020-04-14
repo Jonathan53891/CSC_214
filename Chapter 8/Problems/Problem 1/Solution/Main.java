09cf7e1301f91d41bb906f5c01efc7408465fc7e
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 11:51:21 -0500

    Create Main.java

---
+++ Chapter 8/Problems/Problem 1/Solution/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+  public static void main(String[] args) {
+  	CashRegisterTester.main(args);
+  	CashRegisterTester2.main(args);
+  	//CashRegister.main(args);
+  	//Coin.main(args);
+  }
+}
