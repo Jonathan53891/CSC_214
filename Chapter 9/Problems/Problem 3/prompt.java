51b6229f0e1a3c13d687be8617996404f8301f8a
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 12:03:47 -0500

    Create prompt.java

---
+++ Chapter 9/Problems/Problem 3/prompt.java
@@ -0,0 +1,10 @@
+Implement a superclass Person. 
+Make two classes, Student and Instructor, that inherit from Person. 
+
+A person has a name and a year of birth. 
+A student has a major, and an instructor has a salary. 
+
+Write the class declarations, the constructors, and the methods toString for 
+all classes. 
+
+Supply a test program for these classes and methods. 
