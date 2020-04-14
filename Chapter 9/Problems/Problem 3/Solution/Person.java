0f65b20cb751f8619c40bab3dd3028ab3aa22df1
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 12:04:45 -0500

    Create Person.java

---
+++ Chapter 9/Problems/Problem 3/Solution/Person.java
@@ -0,0 +1,16 @@
+public class Person
+{
+	private String name;
+	private int birthYear;
+
+	public Person(String n, int byear)
+	{
+	  name = n;
+	  birthYear = byear;
+	}
+
+	public String toString()
+	{
+	  return "Person[name=" + name + ",birthYear=" + birthYear + "]";
+	}
+}
