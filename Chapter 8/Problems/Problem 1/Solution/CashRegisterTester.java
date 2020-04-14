b4dee060eb68ac98baf77ac979bc2c04d366f6f3
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 11:51:51 -0500

    Create CashRegisterTester.java

---
+++ Chapter 8/Problems/Problem 1/Solution/CashRegisterTester.java
@@ -0,0 +1,20 @@
+public class CashRegisterTester
+{
+   public static void main(String[] args)
+   {
+      final double NICKEL_VALUE = 0.05;
+      final double DIME_VALUE = 0.1;
+      final double QUARTER_VALUE = 0.25;
+
+      CashRegister myRegister = new CashRegister();
+      myRegister.recordPurchase(0.82);
+      myRegister.receivePayment(3, new Coin(QUARTER_VALUE, "quarter"));
+      myRegister.receivePayment(2, new Coin(NICKEL_VALUE, "nickel"));
+      //myRegister.enterPayment(3, new Coin(QUARTER_VALUE, "quarter"));
+      //myRegister.enterPayment(2, new Coin(NICKEL_VALUE, "nickel"));
+
+      double myChange = myRegister.giveChange();
+      System.out.println("Change: " + myChange);
+      System.out.println("Expected: 0.03");
+   }
+}
