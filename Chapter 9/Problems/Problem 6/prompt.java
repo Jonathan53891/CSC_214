221839bc0f5654029b8bed29a2834e9c054e9ce9
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-14 12:09:34 -0500

    Create prompt.java

---
+++ Chapter 9/Problems/Problem 6/prompt.java
@@ -0,0 +1,3 @@
+ Improve the appointment book program of Chatper 9 problem 5 to give the 
+ user the option to add new appointments. The user must specify the type of 
+ the appointment, the description, and the date. 
