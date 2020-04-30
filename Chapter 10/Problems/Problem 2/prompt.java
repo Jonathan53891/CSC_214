04a0bf2cc6debbf276fdf8800007114474eed34d
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-29 20:53:53 -0500

    Create prompt.java

---
+++ Chapter 10/Problems/Problem 2/prompt.java
@@ -0,0 +1,5 @@
+Implement a class Quiz that implements the Measurable interface of Chapter 
+10 Problem 1 A quiz has a score and a letter grade (such as B+). Modify the 
+Data class from Section 10.1 to process an array of quizzes. Display the 
+average score and the quiz with the highest score (both letter grade and 
+score). 
