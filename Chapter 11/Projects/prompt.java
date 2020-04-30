a686c3207ed65fd55acfda0709c10a2fec4bde2a
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-29 21:05:00 -0500

    Create prompt.java

---
+++ Chapter 11/Projects/prompt.java
@@ -0,0 +1,11 @@
+/*
+
+Write a program that carries out the following tasks:
+
+1. Open a file with the name hello.txt.
+2. Store the message "Hello, World!" in the file.
+3. Close the file.
+4. Open the same file again.
+5. Read the message into a string variable and print it.
+
+*/
