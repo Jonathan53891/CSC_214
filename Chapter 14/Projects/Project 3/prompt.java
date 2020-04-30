995fbfdb075628405517bd650b049f19ddf44421
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-30 14:13:26 -0500

    Create prompt.java

---
+++ Chapter 14/Projects/Project 3/prompt.java
@@ -0,0 +1,9 @@
+Write a telephone lookup program. 
+
+Read a data set of 1,000 names and telephone numbers from a file (directory.txt) that contains the numbers in random order. 
+
+Handle lookups by name and also reverse lookups by phone number. 
+
+Use a binary search for both lookups.
+
+A driver program and templates have been created for you.
