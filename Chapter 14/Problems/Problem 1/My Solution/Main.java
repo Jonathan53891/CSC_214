b7cdded67e95c88c082ad26c60070c3ef382d70c
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-30 14:11:55 -0500

    Create Main.java

---
+++ Chapter 14/Problems/Problem 1/My Solution/Main.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+/**
+ * "Modify the selection sort algorithm to sort an array of integers in descending order."
+ */
+public class Main
+{
+  public static void reverseSort(int[] array)
+  {
+    for(int i=0;i<array.length;i++)
+    {
+      int minPos = findMaxPos(i, array);
+      swap(i, minPos, array);
+    }
+  }
+  
+  private static int findMaxPos(int from, int[] array)
+  {
+    int maxPos=from;
+    for (int i=from;i<array.length;i++)
+    {
+      if(array[i]>array[from])
+      {
+        maxPos=i;
+      }
+    }
+    return maxPos;
+  }
+  
+  private static void swap(int i, int maxPos, int[] array)
+  {
+    int temp=array[maxPos];
+    array[maxPos]=array[i];
+    array[i]=temp;
+  }
+
+  public static void main(String[] args) 
+  {
+    int[] array = {2, 7, 9, 1};
+    System.out.println("Before sort: " + Arrays.toString(array));
+    Main.reverseSort(array);
+    System.out.println("After sort: " + Arrays.toString(array));
+  }
+
+
+}
