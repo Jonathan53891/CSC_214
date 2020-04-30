efe93ccc1281409c36541516fe34906fb4d93251
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-29 20:50:04 -0500

    Create prompt.java

---
+++ Chapter 10/Problems/Problem 1/prompt.java
@@ -0,0 +1,3 @@
+Add static methods largest and smallest to the Measurable interface. The 
+methods should return the object with the largest or smallest measure from 
+an array of Measurable objects. 
