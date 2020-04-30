e7492485a54200b69728fa0f49679c875576b533
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-30 14:11:28 -0500

    Create prompt.java

---
+++ Chapter 14/Problems/Problem 1/prompt.java
@@ -0,0 +1,5 @@
+Modify the selection sort algorithm to sort an array of integers in 
+descending order. 
+
+I've given you the selection sort algorithm from the book here.  Modify it 
+to finish the problem.
