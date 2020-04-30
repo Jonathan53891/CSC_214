722223635518d33149727f977eb4e0eee6de7799
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-29 20:58:45 -0500

    Create prompt.java

---
+++ Chapter 10/Problems/Problem 4/prompt.java
@@ -0,0 +1,7 @@
+ Suppose we have a string holding the text of an entire book. If we want to 
+ analyze segments of the text, it is inefficient to make substrings. Instead, 
+ we should just store a reference to the original text and the starting and 
+ ending positions. Design a class Segment that does this, and have it 
+ implement the CharSequence interface of the standard library. 
+
+Build a test program
