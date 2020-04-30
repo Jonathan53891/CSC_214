4c96ad71f74f9d3dd669f53da96b175f4f972cf7
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-04-30 14:03:45 -0500

    Create BadDataException.java

---
+++ Chapter 11/Projects/Project 5/My Solution/BadDataException.java
@@ -0,0 +1,11 @@
+import java.io.IOException;
+
+public class BadDataException extends IOException
+{
+	public BadDataException() {}
+	
+	public BadDataException(String message)
+	{
+		super(message);
+	}
+}
