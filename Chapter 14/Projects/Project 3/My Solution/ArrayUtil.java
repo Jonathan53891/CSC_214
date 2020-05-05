1203dff37ce3914356802897df306d80f5bb6330
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-05-05 15:30:01 -0500

    Update ArrayUtil.java

--- Chapter 14/Projects/Project 3/My Solution/ArrayUtil.java
+++ Chapter 14/Projects/Project 3/My Solution/ArrayUtil.java
@@ -1,39 +1,25 @@
 import java.util.Random;
 
-/**
- * This class contains utility methods for array manipulation.
- */
 public class ArrayUtil
 {
-    /**
-     * Creates an array filled with random values.
-     *
-     * @param length the length of an array
-     * @param n      the number of possible random values
-     * @return an array filled with length numbers between 0 and n - 1
-     */
-    public static int[] randomIntArray(int length, int n)
+  public static int[] randomIntArray(int length, int n)
+  {
+    int[] a = new int[length];
+    Random generator = new Random();
+    
+    for (int i = 0; i < a.length; i++)
     {
-        int[] a = new int[length];
-        Random generator = new Random();
-
-        for (int i = 0; i < a.length; i++)
-        {
-            a[i] = generator.nextInt(n);
-        }
-        return a;
+      a[i] = generator.nextInt(n);
     }
-
-    /**
-     * Prints all elements in an array.
-     * @param a the array to print
-     */
-    public static void print(int[] a)
+    return a;
+  }
+  
+  public static void print(int[] a)
+  {
+    for (int el : a)
     {
-        for (int el : a)
-        {
-            System.out.print(el + " ");
-        }
-        System.out.println();
+      System.out.print(el + " ");
     }
+    System.out.println();
+  }
 }
