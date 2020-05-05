c63f57a10a203332f3e130dde8849bc37d136ffd
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-05-05 15:31:00 -0500

    Update LookupTable.java

--- Chapter 14/Projects/Project 3/My Solution/LookupTable.java
+++ Chapter 14/Projects/Project 3/My Solution/LookupTable.java
@@ -1,91 +1,66 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 
-/**
- * A {@code LookupTable} for a phone list.
- */
 public class LookupTable
 {
-    private Item[] items;
-    private int itemCount;
+  private Item[] items;
+  private int itemCount;
+  
+  public LookupTable(int n)
+  {
+    items = new Item[n];
+    itemCount = 0;
+  }
+  
+  public void read(BufferedReader in) throws IOException
+  {
+    boolean more = true;
 
-    /**
-     * Constructs a LookupTable object.
-     *
-     * @param n the maximum number of entries
-     */
-    public LookupTable(int n)
+    while (more && itemCount < items.length)
     {
-        items = new Item[n];
-        itemCount = 0;
-    }
+      String k = in.readLine();
+      String v = null;
 
-    /**
-     * Reads a file.
-     *
-     * @param in the file to read
-     * @throws IOException
-     */
-    public void read(BufferedReader in) throws IOException
+      if (k != null)
+      {
+        v = in.readLine();
+      }
+      if (v != null)
+      {
+        items[itemCount] = new Item(k, v);
+        itemCount++;
+      }
+      else
+      {
+        more = false;
+      }
+    }
+    MergeSorter m = new MergeSorter(items);
+    m.sort();
+  }
+  
+  public void reverse(LookupTable b)
+  {
+    itemCount = Math.min(b.itemCount, items.length);
+    for (int i = 0; i < itemCount; i++)
     {
-        boolean more = true;
-
-        while (more && itemCount < items.length)
-        {
-            String k = in.readLine();
-            String v = null;
-
-            if (k != null)
-            {
-                v = in.readLine();
-            }
-            if (v != null)
-            {
-                items[itemCount] = new Item(k, v);
-                itemCount++;
-            }
-            else
-            {
-                more = false;
-            }
-        }
-        MergeSorter m = new MergeSorter(items);
-        m.sort();
+      items[i] = new Item(b.items[i].getValue(), b.items[i].getKey());
     }
-
-    /**
-     * Reverse the lookup by reading phone numbers.
-     *
-     * @param b the LookupTable
-     */
-    public void reverse(LookupTable b)
+    MergeSorter m = new MergeSorter(items);
+    m.sort();
+  }
+  
+  public String lookup(String k)
+  {
+    BinarySearcher b = new BinarySearcher(items);
+    int pos = b.binarySearch(0, itemCount - 1, k);
+    if (pos < 0)
     {
-        itemCount = Math.min(b.itemCount, items.length);
-        for (int i = 0; i < itemCount; i++)
-        {
-            items[i] = new Item(b.items[i].getValue(), b.items[i].getKey());
-        }
-        MergeSorter m = new MergeSorter(items);
-        m.sort();
+      return "Not found";
     }
-
-    /**
-     * Looks up an entries in the list.
-     *
-     * @param k the key to find
-     * @return the item
-     */
-    public String lookup(String k)
+    else
     {
-        BinarySearcher b = new BinarySearcher(items);
-        int pos = b.binarySearch(0, itemCount - 1, k);
-        if (pos < 0)
-        {
-            return "Not found";
-        }
-        else
-        {
-            return items[pos].getValue();
-        }
+      return items[pos].getValue();
     }
+  }
 }
