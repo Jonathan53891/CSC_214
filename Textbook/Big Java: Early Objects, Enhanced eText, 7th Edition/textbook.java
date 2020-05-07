dbd1b260f0aefcf54c50d93d99491bd3cae20dd6
Jonathan Pilafas <jonathan.pilafas@yahoo.com>
2020-05-07 17:14:21 -0500

    Create textbook.java

---
+++ Textbook/Big Java: Early Objects, Enhanced eText, 7th Edition/textbook.java
@@ -0,0 +1,11 @@
+/*
+
+This course will use the following text book:
+
+"Big Java: Early Objects, Enhanced eText, 7th Edition" by Cay Horstman
+
+I rented the e-book off of Chegg so I could interact with the online 
+module and run the various code examples in real-time, but the use of
+this resource is entirely up to the student.
+
+*/
